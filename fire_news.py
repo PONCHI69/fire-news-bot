2714c334ccbca4ee5a8f9345ec7331b8b9920827
PONCHI69 <kingwam69@gmail.com>
2026-01-21 22:36:49 +0800

    Update fire_news.py

--- fire_news.py
+++ fire_news.py
@@ -11,7 +11,7 @@ SEEN_FILE = "seen_events.txt"  # 建議改回文字檔，在 GitHub Actions 存�
 
 FIRE_KEYWORDS = ["fire", "blaze", "火災", "火警", "起火", "燒毀"]
 EXPLOSION_KEYWORDS = ["explosion", "爆炸", "氣爆"]
-FACILITY_KEYWORDS = ["factory", "plant", "mill", "refinery", "warehouse", "工廠", "廠房", "倉儲", "工業","公司"，"科技"，"電子"，"廠"]
+FACILITY_KEYWORDS = ["factory", "plant", "mill", "refinery", "warehouse", "工廠", "廠房", "倉儲", "工業","公司","科技","電子","廠"]
 EXCLUDE_KEYWORDS = ["遊戲", "steam", "限免", "模擬器", "大亨", "缺工", "關稅", "股市", "講座", "論壇"]
 
 # =========================
